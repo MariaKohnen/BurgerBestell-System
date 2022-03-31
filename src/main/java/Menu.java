d74497ca7ec7a674b0a01fb5a32576e8c4ab7e06
maxschemann <schemann@phonetix.net>
2022-03-31 15:52:19 +0200

    Menu-Klasse

---
+++ src/main/java/Menu.java
@@ -0,0 +1,62 @@
+public class Menu {
+    String menuName;
+    double price;
+    String mainDish;
+    String sideDish;
+    String beverage;
+
+    Menu() {
+    }
+
+    Menu(String menuName,
+         double price,
+         String mainDish,
+         String sideDish,
+         String beverage) {
+        this.menuName = menuName;
+        this.price = price;
+        this.mainDish = mainDish;
+        this.sideDish = sideDish;
+        this.beverage = beverage;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getMainDish() {
+        return mainDish;
+    }
+
+    public void setMainDish(String mainDish) {
+        this.mainDish = mainDish;
+    }
+
+    public String getSideDish() {
+        return sideDish;
+    }
+
+    public void setSideDish(String sideDish) {
+        this.sideDish = sideDish;
+    }
+
+    public String getBeverage() {
+        return beverage;
+    }
+
+    public void setBeverage(String beverage) {
+        this.beverage = beverage;
+    }
+}
